5fc2036fbca4b767a6a5c678667663ad4dda4101
azoladyantyi <aizydyantyi@gmail.com>
2017-11-06 15:44:51 +0200

    updated files

--- public/main.js
+++ public/main.js
@@ -1,22 +1,54 @@
-$(function(){
-//introduce template for table
-var tableTemplate = document.querySelector(".tableTemplate").innerHTML;
-//Compile that  into a template
-var tableTemplateInst = Handlebars.compile(tableTemplate);
-var displayTableStock = document.getElementById('displayTableStock');
+$(function() {
+    //introduce template for table
+    var tableTemplate = document.querySelector(".tableTemplate").innerHTML;
+    //Compile that  into a template
+    var tableTemplateInst = Handlebars.compile(tableTemplate);
+    var displayTableStock = document.getElementById('displayTableStock');
+    var addBtn = document.getElementById('addBtn')
 
 
     $.ajax({
         type: "GET",
         url: "/api/shoes",
         dataType: 'json',
-        success: function(oders){
+        success: function(oders) {
             displayTableStock.innerHTML = tableTemplateInst({
                 shoes: oders.data
-              })
+            })
 
 
         }
 
     });
-});
+
+$("#addBtn").click(function() {
+
+  var new_color = document.getElementById("new_color");
+  var new_size = document.getElementById("new_size");
+  var new_brand = document.getElementById("new_brand");
+  var new_price = document.getElementById("new_price");
+  var new_stock = document.getElementById("new_stock");
+
+  var newOjbShoes = {
+    brand: new_brand.value,
+    size: new_size.value,
+    color: new_color.value,
+    price: new_price.value,
+    in_stock: new_stock.value
+  }
+
+
+    $.ajax({
+        type: "POST",
+        url: "/api/shoes",
+        data: newOjbShoes,
+        dataType: 'json',
+        success: function(oders) {
+
+            }
+
+
+        })
+
+    });
+        });
